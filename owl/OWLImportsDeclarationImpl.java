2bb71fc2c7cc48520645414ffb6ea9adaac245cd
matthewhorridge <matthewhorridge@02e1a946-7720-0410-8c3b-a7e05feb851e>
2009-03-25 09:36:11 +0000

    git-svn-id: https://owlapi.svn.sourceforge.net/svnroot/owlapi/v3/trunk@1066 02e1a946-7720-0410-8c3b-a7e05feb851e

--- owl/OWLImportsDeclarationImpl.java
+++ owl/OWLImportsDeclarationImpl.java
@@ -57,6 +57,10 @@ public class OWLImportsDeclarationImpl implements OWLImportsDeclaration {
     }
 
 
+    public int hashCode() {
+        return iri.hashCode();
+    }
+
 
     public boolean equals(Object obj) {
         if(obj == this) {
@@ -70,43 +74,7 @@ public class OWLImportsDeclarationImpl implements OWLImportsDeclaration {
     }
 
 
-    /**
-     * Compares this object with the specified object for order.  Returns a
-     * negative integer, zero, or a positive integer as this object is less
-     * than, equal to, or greater than the specified object.<p>
-     *
-     * In the foregoing description, the notation
-     * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
-     * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
-     * <tt>0</tt>, or <tt>1</tt> according to whether the value of <i>expression</i>
-     * is negative, zero or positive.
-     *
-     * The implementor must ensure <tt>sgn(x.compareTo(y)) ==
-     * -sgn(y.compareTo(x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
-     * implies that <tt>x.compareTo(y)</tt> must throw an exception iff
-     * <tt>y.compareTo(x)</tt> throws an exception.)<p>
-     *
-     * The implementor must also ensure that the relation is transitive:
-     * <tt>(x.compareTo(y)&gt;0 &amp;&amp; y.compareTo(z)&gt;0)</tt> implies
-     * <tt>x.compareTo(z)&gt;0</tt>.<p>
-     *
-     * Finally, the implementer must ensure that <tt>x.compareTo(y)==0</tt>
-     * implies that <tt>sgn(x.compareTo(z)) == sgn(y.compareTo(z))</tt>, for
-     * all <tt>z</tt>.<p>
-     *
-     * It is strongly recommended, but <i>not</i> strictly required that
-     * <tt>(x.compareTo(y)==0) == (x.equals(y))</tt>.  Generally speaking, any
-     * class that implements the <tt>Comparable</tt> interface and violates
-     * this condition should clearly indicate this fact.  The recommended
-     * language is "Note: this class has a natural ordering that is
-     * inconsistent with equals."
-     * @param o the Object to be compared.
-     * @return a negative integer, zero, or a positive integer as this object
-     *         is less than, equal to, or greater than the specified object.
-     * @throws ClassCastException if the specified object's type prevents it
-     * from being compared to this Object.
-     */
     public int compareTo(OWLImportsDeclaration o) {
-        return getIRI().compareTo(o.getIRI());
+        return iri.compareTo(o.getIRI());
     }
 }
