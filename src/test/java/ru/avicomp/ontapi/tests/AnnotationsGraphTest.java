b55da442f97b423ed2adc3494b0ce8032fe90809
szuev <szuev@avicomp.ru>
2016-10-11 18:44:49 +0300

    ont-api: add a couple of tests for annotations

---
+++ src/test/java/ru/avicomp/ontapi/tests/AnnotationsGraphTest.java
@@ -0,0 +1,139 @@
+package ru.avicomp.ontapi.tests;
+
+import java.util.Iterator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.vocabulary.OWL;
+import org.apache.jena.vocabulary.OWL2;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
+import org.junit.Assert;
+import org.junit.Test;
+import org.semanticweb.owlapi.model.*;
+
+import ru.avicomp.ontapi.OntManagerFactory;
+import ru.avicomp.ontapi.OntologyModel;
+import ru.avicomp.ontapi.utils.OntIRI;
+
+/**
+ * test for annotations.
+ * <p>
+ * Created by szuev on 11.10.2016.
+ */
+public class AnnotationsGraphTest extends GraphTestBase {
+
+    @Test
+    public void test1() throws OWLOntologyCreationException {
+        OntIRI iri = OntIRI.create("http://test.org/annotations/1");
+        OWLOntologyManager manager = OntManagerFactory.createOWLOntologyManager();
+
+        // test data:
+        OntIRI clazzIRI = iri.addFragment("SomeClass1");
+        OntIRI annotationProperty = iri.addFragment("some-annotation-property");
+        String comment = "comment here";
+        String commentLang = "s";
+        String label = "some-label";
+
+        LOGGER.info("Create fresh ontology (" + iri + ").");
+        OntologyModel owl = (OntologyModel) manager.createOntology(iri.toOwlOntologyID());
+        OntModel jena = owl.asGraphModel();
+        OntClass ontClass = jena.createClass(clazzIRI.getIRIString());
+
+        LOGGER.info("Assemble annotations using jena.");
+        Resource commentURI;
+        Literal label4, label2;
+        Resource root = jena.createResource();
+        jena.add(root, RDF.type, OWL2.Annotation);
+        jena.add(root, OWL2.annotatedProperty, RDFS.label);
+        jena.add(root, OWL2.annotatedTarget, label4 = ResourceFactory.createLangLiteral(comment, commentLang));
+        jena.add(root, RDFS.comment, commentURI = ResourceFactory.createResource(annotationProperty.getIRIString()));
+        jena.add(root, RDFS.label, label2 = ResourceFactory.createPlainLiteral(label));
+        Resource anon = jena.createResource();
+        jena.add(root, OWL2.annotatedSource, anon);
+        jena.add(anon, RDF.type, OWL2.Axiom);
+        jena.add(anon, OWL2.annotatedSource, ontClass);
+        jena.add(anon, OWL2.annotatedProperty, RDF.type);
+        jena.add(anon, OWL2.annotatedTarget, OWL.Class);
+        jena.add(anon, RDFS.comment, commentURI);
+        jena.add(anon, RDFS.label, label2);
+        jena.add(anon, RDFS.label, label4);
+
+        jena.rebind();
+        debug(owl);
+
+        LOGGER.info("Check");
+        OWLDataFactory factory = manager.getOWLDataFactory();
+        OWLAxiom expected = factory.getOWLDeclarationAxiom(factory.getOWLClass(clazzIRI), Stream.of(
+                factory.getOWLAnnotation(factory.getRDFSComment(), annotationProperty),
+                factory.getOWLAnnotation(factory.getRDFSLabel(), factory.getOWLLiteral(comment, commentLang), Stream.of(
+                        factory.getOWLAnnotation(factory.getRDFSComment(), annotationProperty),
+                        factory.getOWLAnnotation(factory.getRDFSLabel(), factory.getOWLLiteral(label))
+                )),
+                factory.getOWLAnnotation(factory.getRDFSLabel(), factory.getOWLLiteral(label))
+        ).collect(Collectors.toSet()));
+        compareAxioms(Stream.of(expected), owl.axioms());
+    }
+
+    @Test
+    public void test2() throws OWLOntologyCreationException {
+        OntIRI iri = OntIRI.create("http://test.org/annotations/2");
+        OWLOntologyManager manager = OntManagerFactory.createOWLOntologyManager();
+        OWLDataFactory factory = manager.getOWLDataFactory();
+        long count = manager.ontologies().count();
+
+        OWLOntologyID id1 = iri.toOwlOntologyID(iri.addPath("1.0"));
+        LOGGER.info("Create ontology " + id1);
+        OntologyModel owl1 = (OntologyModel) manager.createOntology(id1);
+
+        OWLAnnotation simple1 = factory.getOWLAnnotation(factory.getRDFSLabel(), factory.getOWLLiteral("PLAIN-1"));
+        OWLAnnotation simple2 = factory.getOWLAnnotation(factory.getRDFSLabel(), factory.getOWLLiteral("PLAIN-2"));
+
+        OWLAnnotation root1child2child1child1 = factory.getOWLAnnotation(factory.getRDFSSeeAlso(), factory.getOWLLiteral("ROOT1->CHILD2->CHILD1->CHILD1 (NIL)"));
+        OWLAnnotation root1child2child1child2 = factory.getOWLAnnotation(factory.getRDFSSeeAlso(), factory.getOWLLiteral("ROOT1->CHILD2->CHILD1->CHILD2 (NIL)"));
+
+        OWLAnnotation root1child2child1 = factory.getOWLAnnotation(factory.getRDFSIsDefinedBy(), factory.getOWLLiteral("ROOT1->CHILD2->CHILD1"), Stream.of(root1child2child1child1, root1child2child1child2));
+        OWLAnnotation root1child2child2 = factory.getOWLAnnotation(factory.getRDFSIsDefinedBy(), factory.getOWLLiteral("ROOT1->CHILD2->CHILD2 (NIL)"));
+
+        OWLAnnotation root1child1child1 = factory.getOWLAnnotation(factory.getRDFSSeeAlso(), factory.getOWLLiteral("ROOT1->CHILD1->CHILD1 (NIL)"));
+        OWLAnnotation root1child1 = factory.getOWLAnnotation(factory.getRDFSSeeAlso(), factory.getOWLLiteral("ROOT1->CHILD1"), root1child1child1);
+        OWLAnnotation root1child2 = factory.getOWLAnnotation(factory.getRDFSLabel(), factory.getOWLLiteral("ROOT1->CHILD2"), Stream.of(root1child2child1, root1child2child2));
+        OWLAnnotation root1 = factory.getOWLAnnotation(factory.getRDFSIsDefinedBy(), factory.getOWLLiteral("ROOT1"), Stream.of(root1child2, root1child1));
+
+        OWLAnnotation root2child1 = factory.getOWLAnnotation(factory.getRDFSComment(), factory.getOWLLiteral("ROOT2->CHILD1 (NIL)"));
+        OWLAnnotation root2 = factory.getOWLAnnotation(factory.getRDFSLabel(), factory.getOWLLiteral("ROOT2"), root2child1);
+
+        OWLClass owlClass = factory.getOWLClass(iri.addFragment("SomeClass1"));
+        OWLAxiom axiom = factory.getOWLDeclarationAxiom(owlClass, Stream.of(root1, root2, simple2, simple1).collect(Collectors.toSet()));
+        owl1.applyChange(new AddAxiom(owl1, axiom));
+
+        OWLAnnotation indiAnn = factory.getOWLAnnotation(factory.getRDFSComment(), factory.getOWLLiteral("INDI-ANN"));
+        OWLAxiom indiAxiom = factory.getOWLClassAssertionAxiom(owlClass, factory.getOWLNamedIndividual(iri.addFragment("Indi")), Stream.of(indiAnn).collect(Collectors.toSet()));
+        owl1.applyChange(new AddAxiom(owl1, indiAxiom));
+
+        debug(owl1);
+
+        OWLOntologyID id2 = iri.toOwlOntologyID(iri.addPath("2.0"));
+        LOGGER.info("Create ontology " + id2 + " (empty)");
+        OntologyModel owl2 = (OntologyModel) manager.createOntology(id2);
+        Assert.assertEquals("Incorrect number of ontologies.", count + 2, manager.ontologies().count());
+
+        LOGGER.info("Pass all content from " + id1 + " to " + id2 + " using jena");
+        OntModel source = owl1.asGraphModel();
+        OntModel target = owl2.asGraphModel();
+        Iterator<Statement> toCopy = source.getBaseModel().listStatements().filterDrop(statement -> iri.toResource().equals(statement.getSubject()));
+        toCopy.forEachRemaining(target::add);
+        target.setNsPrefixes(source.getNsPrefixMap()); // just in case
+        target.rebind();
+        debug(owl2);
+
+        LOGGER.info("Compare axioms"); // note! there is one more axiom in new ontology: Declaration(NamedIndividual(<http://test.org/annotations/2#Indi>))
+        compareAxioms(owl1.axioms(), owl2.axioms().filter(OWLAxiom::isAnnotated));
+    }
+}
