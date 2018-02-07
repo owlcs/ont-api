6b0284b3006720dfdcc46a9659553c0219637295
szuev <szuev@avicomp.ru>
2018-02-07 13:38:28 +0300

    ont-api: internal-config-provider - remove uk.ac.manchester.cs.owl.owlapi dependency (in bounds of issue https://github.com/avicomp/ont-api/issues/10)

--- src/main/java/ru/avicomp/ontapi/internal/ConfigProvider.java
+++ src/main/java/ru/avicomp/ontapi/internal/ConfigProvider.java
@@ -17,10 +17,10 @@ package ru.avicomp.ontapi.internal;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLOntologyWriterConfiguration;
 
+import ru.avicomp.ontapi.OntManagers;
 import ru.avicomp.ontapi.config.OntConfig;
 import ru.avicomp.ontapi.config.OntLoaderConfiguration;
 import ru.avicomp.ontapi.config.OntWriterConfiguration;
-import uk.ac.manchester.cs.owl.owlapi.OWLDataFactoryImpl;
 
 /**
  * This is an internal object to provide access to the {@link Config} with access to the OWL-API containers with settings.
@@ -82,7 +82,7 @@ public interface ConfigProvider {
      * Default (dummy) implementation of {@link Config}.
      */
     class Dummy implements Config {
-        private static final OWLDataFactory DATA_FACTORY = new OWLDataFactoryImpl();
+        private static final OWLDataFactory DATA_FACTORY = OntManagers.Profile.DEFAULT_DATA_FACTORY;
         private static final OntConfig GLOBAL_CONFIG = new OntConfig();
         private static final OntLoaderConfiguration LOADER_CONFIGURATION = GLOBAL_CONFIG.buildLoaderConfiguration();
         private static final OntWriterConfiguration WRITER_CONFIGURATION = GLOBAL_CONFIG.buildWriterConfiguration();
