d9157a71ece7f0fa202c9626c6b5f5bd5cadfca4
szuev <sss.zuev@gmail.com>
2018-03-09 17:02:48 +0300

    ont-api: add tests for issue https://github.com/avicomp/ont-api/issues/12 (splitted sub-annotations)

--- src/test/java/ru/avicomp/ontapi/tests/model/AnnotationsOntModelTest.java
+++ src/test/java/ru/avicomp/ontapi/tests/model/AnnotationsOntModelTest.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of the ONT API.
  * The contents of this file are subject to the LGPL License, Version 3.0.
- * Copyright (c) 2017, Avicomp Services, AO
+ * Copyright (c) 2018, Avicomp Services, AO
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
@@ -10,6 +10,7 @@
  * Alternatively, the contents of this file may be used under the terms of the Apache License, Version 2.0 in which case, the provisions of the Apache License Version 2.0 are applicable instead of those above.
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *
  */
 
 package ru.avicomp.ontapi.tests.model;
@@ -397,6 +398,46 @@ public class AnnotationsOntModelTest extends OntModelTestBase {
     @Test
     public void testAnnotateOntology() throws Exception {
         annotateOntologyTest(OntManagers.createONT());
+    }
+
+    @Test
+    public void testLoadCommonBulkAnnotations() throws Exception {
+        OWLOntologyManager m = OntManagers.createONT();
+        OWLDataFactory df = m.getOWLDataFactory();
+        OWLAnnotationProperty p1 = df.getOWLAnnotationProperty("http://www.geneontology.org/formats/oboInOwl#hasNarrowSynonym");
+        OWLAnnotationProperty p2 = df.getOWLAnnotationProperty("http://www.geneontology.org/formats/oboInOwl#hasDbXref");
+        OWLClass c = df.getOWLClass("http://purl.obolibrary.org/obo/TTO_1006537");
+
+        OWLOntology o = m.loadOntologyFromOntologyDocument(IRI.create(ReadWriteUtils.getResourceURI("test-annotations-1.ttl")));
+        Assert.assertEquals("Wrong annotation assertions count", 1, o.axioms(AxiomType.ANNOTATION_ASSERTION).count());
+        @SuppressWarnings("ConstantConditions") OWLAnnotationAssertionAxiom axiom = o.axioms(AxiomType.ANNOTATION_ASSERTION).findFirst().get();
+        Assert.assertEquals("Wrong value", df.getOWLLiteral("Squalus cinereus"), axiom.getValue());
+        Assert.assertEquals("Wrong subject", c.getIRI(), axiom.getSubject());
+        Assert.assertEquals("Wrong predicate", p1, axiom.getProperty());
+        List<OWLAnnotation> sub = axiom.annotationsAsList();
+        Assert.assertEquals("Wrong count of sub-annotations", 2, sub.size());
+        sub.forEach(a -> Assert.assertEquals("Wrong predicate", p2, a.getProperty()));
+    }
+
+    @Test
+    public void testLoadSplitBulkAnnotations() throws Exception {
+        OWLOntologyManager m = OntManagers.createONT();
+        OWLDataFactory df = m.getOWLDataFactory();
+        OWLAnnotationProperty p1 = df.getOWLAnnotationProperty("http://www.geneontology.org/formats/oboInOwl#hasNarrowSynonym");
+        OWLAnnotationProperty p2 = df.getOWLAnnotationProperty("http://www.geneontology.org/formats/oboInOwl#hasDbXref");
+        OWLClass c = df.getOWLClass("http://purl.obolibrary.org/obo/TTO_1006537");
+
+        OWLOntology o = m.loadOntologyFromOntologyDocument(IRI.create(ReadWriteUtils.getResourceURI("test-annotations-2.ttl")));
+        Assert.assertEquals("Wrong annotation assertions count", 2, o.axioms(AxiomType.ANNOTATION_ASSERTION).count());
+
+        o.axioms(AxiomType.ANNOTATION_ASSERTION).forEach(axiom -> {
+            Assert.assertEquals("Wrong value", df.getOWLLiteral("Squalus cinereus"), axiom.getValue());
+            Assert.assertEquals("Wrong subject", c.getIRI(), axiom.getSubject());
+            Assert.assertEquals("Wrong predicate", p1, axiom.getProperty());
+            List<OWLAnnotation> sub = axiom.annotationsAsList();
+            Assert.assertEquals("Wrong count of sub-annotations", 1, sub.size());
+            Assert.assertEquals("Wrong predicate", p2, sub.get(0).getProperty());
+        });
     }
 
     private void annotateOntologyTest(OWLOntologyManager manager) throws Exception {
