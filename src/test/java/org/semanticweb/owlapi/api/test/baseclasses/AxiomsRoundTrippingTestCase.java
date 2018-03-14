32812be3e8a61c092375394985f524b436c4b2a4
szuev <sss.zuev@gmail.com>
2018-03-14 22:47:08 +0300

    ont-api: tests, cosmetic changes related to https://github.com/avicomp/ont-api/issues/13

--- src/test/java/org/semanticweb/owlapi/api/test/baseclasses/AxiomsRoundTrippingTestCase.java
+++ src/test/java/org/semanticweb/owlapi/api/test/baseclasses/AxiomsRoundTrippingTestCase.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of the ONT API.
  * The contents of this file are subject to the LGPL License, Version 3.0.
- * Copyright (c) 2017, Avicomp Services, AO
+ * Copyright (c) 2018, Avicomp Services, AO
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
@@ -10,21 +10,22 @@
  * Alternatively, the contents of this file may be used under the terms of the Apache License, Version 2.0 in which case, the provisions of the Apache License Version 2.0 are applicable instead of those above.
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *
  */
 package org.semanticweb.owlapi.api.test.baseclasses;
 
-import java.util.*;
-import java.util.stream.Stream;
-
+import com.google.common.collect.Sets;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.vocab.OWL2Datatype;
 import org.semanticweb.owlapi.vocab.OWLFacet;
 
-import com.google.common.collect.Sets;
+import java.util.*;
+import java.util.stream.Stream;
 
 import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.*;
 import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.Class;
@@ -103,7 +104,8 @@ public class AxiomsRoundTrippingTestCase extends AxiomsRoundTrippingBase {
 
     @Parameters
     public static List<AxiomBuilder> getData() {
-        return Arrays.asList(
+        //noinspection RedundantTypeArguments (explicit type arguments speedup compilation and analysis time)
+        return Arrays.<AxiomBuilder>asList(
                 // SWRLRuleAlternateNS
                 () -> {
                     Set<OWLAxiom> axioms = new HashSet<>();
@@ -220,7 +222,7 @@ public class AxiomsRoundTrippingTestCase extends AxiomsRoundTrippingBase {
                 () -> Sets.newHashSet(HasKey(singleton(Annotation(apropA, Literal("Test", ""))), clsA, propA, propB, propC),
                         Declaration(apropA), Declaration(propA), Declaration(propB), Declaration(propC)),
                 //
-                () -> singleton(DisjointClasses(asUnorderedSet(Stream.generate(() -> createClass()).limit(1000)))),
+                () -> singleton(DisjointClasses(asUnorderedSet(Stream.generate(OWLFunctionalSyntaxFactory::createClass).limit(1000)))),
                 //
                 () -> singleton(SubClassOf(clsB, ObjectSomeValuesFrom(op.getInverseProperty(), clsA))),
                 //
@@ -266,7 +268,7 @@ public class AxiomsRoundTrippingTestCase extends AxiomsRoundTrippingBase {
                 //
                 () -> singleton(IrreflexiveObjectProperty(op)),
                 //
-                () -> singleton(DifferentIndividuals(asUnorderedSet(Stream.generate(() -> createIndividual()).limit(
+                () -> singleton(DifferentIndividuals(asUnorderedSet(Stream.generate(OWLFunctionalSyntaxFactory::createIndividual).limit(
                         1000)))),
                 //
                 () -> Sets.newHashSet(AnnotationAssertion(apropA, clsA.getIRI(), Literal("abc", "en")), Declaration(clsA)),
