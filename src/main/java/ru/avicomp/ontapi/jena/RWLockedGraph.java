9dd43a65bbeb165d5fecdc3e0c88f30a1bcbf524
szuev <sss.zuev@gmail.com>
2019-04-06 16:11:06 +0300

    ont-api: RWLockedGraph - thread safety on iteration (issue #65) + test

--- src/main/java/ru/avicomp/ontapi/jena/RWLockedGraph.java
+++ src/main/java/ru/avicomp/ontapi/jena/RWLockedGraph.java
@@ -15,29 +15,85 @@
 package ru.avicomp.ontapi.jena;
 
 import org.apache.jena.graph.*;
+import org.apache.jena.mem.GraphMem;
 import org.apache.jena.shared.AddDeniedException;
 import org.apache.jena.shared.DeleteDeniedException;
 import org.apache.jena.shared.PrefixMapping;
 import org.apache.jena.util.iterator.ExtendedIterator;
+import org.apache.jena.util.iterator.FilterIterator;
+import org.apache.jena.util.iterator.WrappedIterator;
 import ru.avicomp.ontapi.jena.utils.Graphs;
+import ru.avicomp.ontapi.jena.utils.Iter;
 
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.locks.ReadWriteLock;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
- * A Graph-Wrapper with {@link ReadWriteLock} inside (the OWL-API synchronization style).
- * TODO: lazy iterators (see {@link #find(Triple)}, {@link #find(Node, Node, Node)}) are not synchronised
- * TODO: the related objects (such as {@link PrefixMapping}) are not synchronized
+ * A {@code Graph} Wrapper with {@link ReadWriteLock} inside (that is the OWL-API synchronization style).
+ * There is also an additional synchronization type that ensures the thread-safety of using iterators.
+ * This is necessary since {@link Iterator iterator}s are lazy in nature.
+ * <p>
+ * Note: the current implementation may temporarily put the iterator data in memory,
+ * for more details see the {@link #find(Triple)} description.
+ * <p>
+ * Also note: although this {@code Graph} is expected to be thread safe,
+ * it does not mean that a model, that wraps this graph, is also will be safe in multithreading:
+ * it is possible to receive the data that will be invalid in the next moment,
+ * which may lead to an exception during the construction of the final {@link org.apache.jena.rdf.model.Resource}.
+ * For example, constructing an {@link ru.avicomp.ontapi.jena.model.OntClass OWL Class} may be failed,
+ * since the root triple {@code _:x rdf:type owl:Class} may be deleted by another thread
+ * immediately after retrieving it.
  * <p>
  * Created by @szuev on 07.04.2017.
  */
+@SuppressWarnings("WeakerAccess")
 public class RWLockedGraph implements Graph {
+    /**
+     * The base {@link Graph}.
+     */
     protected final Graph base;
+    /**
+     * {@link ReadWriteLock} to ensure thread-safety.
+     */
     protected final ReadWriteLock lock;
+    /**
+     * The delay in milliseconds, that is used to select {@link ExtendedIterator} processing strategy.
+     */
+    protected final long delay;
+    /**
+     * The {@code Collection} of all {@link ExtendedIterator}, as {@link WeakHashMap},
+     * since it is possible that some iterator was simply forgotten.
+     */
+    protected final Collection<WIT<?>> iterators = Collections.newSetFromMap(new WeakHashMap<>());
 
+    /**
+     * Constructs a new {@link RWLockedGraph Read/Write Locked Graph Wrapper}.
+     *
+     * @param base {@link Graph}, not {@code null}
+     * @param lock {@link ReadWriteLock}, not {@code null}
+     */
     public RWLockedGraph(Graph base, ReadWriteLock lock) {
+        this(base, lock, 500);
+    }
+
+    /**
+     * Constructs a new {@link RWLockedGraph Read/Write Locked Graph Wrapper}.
+     *
+     * @param base                {@link Graph}, not {@code null}
+     * @param lock                {@link ReadWriteLock}, not {@code null}
+     * @param delayInMilliseconds long, positive number
+     * @throws RuntimeException if wrong input parameters
+     */
+    public RWLockedGraph(Graph base, ReadWriteLock lock, long delayInMilliseconds) {
         this.base = Objects.requireNonNull(base, "Null base graph");
         this.lock = Objects.requireNonNull(lock, "Null lock");
+        if (delayInMilliseconds <= 0)
+            throw new IllegalArgumentException("Non-positive delay specified.");
+        this.delay = delayInMilliseconds;
     }
 
     public Graph get() {
@@ -49,19 +105,12 @@ public class RWLockedGraph implements Graph {
     }
 
     @Override
-    public boolean dependsOn(Graph other) {
-        lock.readLock().lock();
-        try {
-            return this == other || Graphs.dependsOn(base, other);
-        } finally {
-            lock.readLock().unlock();
-        }
-    }
-
-    @Override
     public void add(Triple t) throws AddDeniedException {
         lock.writeLock().lock();
         try {
+            if (!base.getCapabilities().addAllowed())
+                throw new AddDeniedException("Attempt to add triple " + t);
+            waitForEmptyIterators();
             base.add(t);
         } finally {
             lock.writeLock().unlock();
@@ -72,56 +121,79 @@ public class RWLockedGraph implements Graph {
     public void delete(Triple t) throws DeleteDeniedException {
         lock.writeLock().lock();
         try {
+            if (!base.getCapabilities().deleteAllowed())
+                throw new DeleteDeniedException("Attempt to delete triple " + t);
+            waitForEmptyIterators();
             base.delete(t);
         } finally {
             lock.writeLock().unlock();
         }
     }
 
+    @Override
+    public void remove(Node s, Node p, Node o) {
+        lock.writeLock().lock();
+        try {
+            if (!base.getCapabilities().deleteAllowed())
+                throw new DeleteDeniedException("Attempt to remove triple " + Triple.createMatch(s, p, o));
+            waitForEmptyIterators();
+            base.remove(s, p, o);
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    @Override
+    public void clear() {
+        lock.writeLock().lock();
+        try {
+            if (!base.getCapabilities().deleteAllowed())
+                throw new DeleteDeniedException("Attempt to clear");
+            waitForEmptyIterators();
+            base.clear();
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
     /**
-     * {@inheritDoc}
-     * Although it is a lazy iterator,
-     * it seems possible to make it safe with R/W locking
-     * at least in straightforward case of a simple iterating one by one.
-     * TODO: implement {@link ExtendedIterator} with small in-memory dump and R/W lock inside
+     * Returns an {@link ExtendedIterator extended iterator} over all the {@link Triple}s that match the triple pattern.
+     * <p>
+     * The return iterator is expected to be thread safe: if graph changes have occurred before the end of iteration
+     * it may return data from the past using their internal snapshot.
+     * In single-thread environment its improper use may lead to {@link ConcurrentModificationException}.
+     * To avoid this, {@link org.apache.jena.util.iterator.ClosableIterator#close() close} the iterator explicitly
+     * if it is not exhausted and don't mix mutation operations with partial iteration.
      *
-     * @param m {@link Triple}, matching a pattern
-     * @return {@link ExtendedIterator} of {@link Triple}s
+     * @param m a {@link Triple triple} encoding the pattern to look for, not {@code null}
+     * @return {@link ExtendedIterator} of all {@link Triple triple}s in this graph that match {@code m}
+     * @see #find(Triple)
      */
     @Override
     public ExtendedIterator<Triple> find(Triple m) {
         lock.readLock().lock();
         try {
-            return base.find(m);
+            return new WIT<>(base.find(m));
         } finally {
             lock.readLock().unlock();
         }
     }
 
     /**
-     * {@inheritDoc}
-     * TODO: see {@link #find(Triple)}
+     * Returns an iterator over Triples matching a pattern.
+     * For more details see the {@link #find(Triple)} description.
      *
-     * @param s {@link Node}, uri or blank
-     * @param p {@link Node}, uri
-     * @param o {@link Node}, uri, blank or literal
+     * @param s {@link Node}, uri or blank, not {@code null}
+     * @param p {@link Node}, uri, not {@code null}
+     * @param o {@link Node}, uri, blank or literal, not {@code null}
      * @return {@link ExtendedIterator} of {@link Triple}s
+     * @see #find(Node, Node, Node)
      */
     @Override
     public ExtendedIterator<Triple> find(Node s, Node p, Node o) {
         lock.readLock().lock();
         try {
-            return base.find(s, p, o);
-        } finally {
-            lock.readLock().unlock();
-        }
-    }
-
-    @Override
-    public boolean isIsomorphicWith(Graph g) {
-        lock.readLock().lock();
-        try {
-            return base.isIsomorphicWith(g);
+            return new WIT<>(base.find(s, p, o));
         } finally {
             lock.readLock().unlock();
         }
@@ -148,22 +220,30 @@ public class RWLockedGraph implements Graph {
     }
 
     @Override
-    public void clear() {
-        lock.writeLock().lock();
+    public boolean isIsomorphicWith(Graph other) {
+        lock.readLock().lock();
         try {
-            base.clear();
+            if (this == other) return true;
+            if (other instanceof RWLockedGraph) {
+                other = ((RWLockedGraph) other).get();
+            }
+            return base.isIsomorphicWith(other);
         } finally {
-            lock.writeLock().unlock();
+            lock.readLock().unlock();
         }
     }
 
     @Override
-    public void remove(Node s, Node p, Node o) {
-        lock.writeLock().lock();
+    public boolean dependsOn(Graph other) {
+        lock.readLock().lock();
         try {
-            base.remove(s, p, o);
+            if (this == other) return true;
+            if (other instanceof RWLockedGraph) {
+                other = ((RWLockedGraph) other).get();
+            }
+            return Graphs.dependsOn(base, other);
         } finally {
-            lock.writeLock().unlock();
+            lock.readLock().unlock();
         }
     }
 
@@ -181,7 +261,9 @@ public class RWLockedGraph implements Graph {
     public boolean isEmpty() {
         lock.readLock().lock();
         try {
-            return base.isEmpty();
+            if (base instanceof GraphMem)
+                return base.isEmpty();
+            return !Iter.findFirst(find()).isPresent();
         } finally {
             lock.readLock().unlock();
         }
@@ -208,16 +290,6 @@ public class RWLockedGraph implements Graph {
     }
 
     @Override
-    public TransactionHandler getTransactionHandler() {
-        lock.readLock().lock();
-        try {
-            return base.getTransactionHandler();
-        } finally {
-            lock.readLock().unlock();
-        }
-    }
-
-    @Override
     public Capabilities getCapabilities() {
         lock.readLock().lock();
         try {
@@ -228,9 +300,21 @@ public class RWLockedGraph implements Graph {
     }
 
     @Override
+    public TransactionHandler getTransactionHandler() {
+        lock.readLock().lock();
+        try {
+            // todo: not thread-safe
+            return base.getTransactionHandler();
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
     public GraphEventManager getEventManager() {
         lock.readLock().lock();
         try {
+            // todo: not thread-safe
             return base.getEventManager();
         } finally {
             lock.readLock().unlock();
@@ -241,6 +325,7 @@ public class RWLockedGraph implements Graph {
     public GraphStatisticsHandler getStatisticsHandler() {
         lock.readLock().lock();
         try {
+            // todo: not thread-safe
             return base.getStatisticsHandler();
         } finally {
             lock.readLock().unlock();
@@ -251,9 +336,253 @@ public class RWLockedGraph implements Graph {
     public PrefixMapping getPrefixMapping() {
         lock.readLock().lock();
         try {
+            // todo: not thread-safe
             return base.getPrefixMapping();
         } finally {
             lock.readLock().unlock();
         }
     }
+
+    /**
+     * Returns the current time in milliseconds to use as timestamp inside {@link WIT}-iterators.
+     *
+     * @return long
+     */
+    public static long currentTimeInMilliSeconds() {
+        return System.currentTimeMillis();
+    }
+
+    /**
+     * Waits for all other (that are created by other threads) iterators.
+     * After operation is finished, all iterators must be either done or cached in the from of snapshot.
+     * The criteria to choose strategy is {@link WIT#timestamp},
+     * if no one has worked with it for more than {@link #delay} milliseconds,
+     * then all its elements will be put in memory using the {@link #makeIteratorSnapshot(WIT)} method.
+     * Testing shows that only about {@code 1%} of iterators fall into memory,
+     * but this number may vary depending on the environment.
+     */
+    protected void waitForEmptyIterators() {
+        Collection<WIT<?>> iterators;
+        while (!(iterators = findOtherIterators()).isEmpty()) {
+            selectOldestIterator(iterators).ifPresent(this::makeIteratorSnapshot);
+        }
+    }
+
+    /**
+     * Makes an in-memory snapshot for the given {@link WIT}-iterator.
+     *
+     * @param it {@link WIT}
+     */
+    @SuppressWarnings("unchecked")
+    protected void makeIteratorSnapshot(WIT<?> it) {
+        ExtendedIterator base = it.setBase(null);
+        ArrayList res = new ArrayList();
+        while (base.hasNext()) {
+            res.add(base.next());
+        }
+        res.trimToSize();
+        it.setBase(WrappedIterator.create(res.iterator()));
+        removeIterator(it);
+    }
+
+    /**
+     * Selects the oldest iterator.
+     *
+     * @param iterators {@link Collection}
+     * @return {@code Optional} around {@link WIT}
+     * @see #delay
+     */
+    protected Optional<WIT<?>> selectOldestIterator(Collection<WIT<?>> iterators) {
+        return iterators.stream()
+                .filter(WIT::isExpired)
+                .min(Comparator.comparingLong(WIT::timestamp));
+    }
+
+    /**
+     * Returns iterators that were created by other threads.
+     *
+     * @return {@code Collection} of {@link WIT}
+     */
+    protected Collection<WIT<?>> findOtherIterators() {
+        synchronized (iterators) {
+            if (iterators.isEmpty()) return Collections.emptySet();
+            Thread th = Thread.currentThread();
+            return iterators.stream().filter(x -> th != x.thread).collect(Collectors.toSet());
+        }
+    }
+
+    /**
+     * Registers the given iterator into the internal {@code Map}-store.
+     *
+     * @param it {@link WIT}
+     */
+    protected void putIterator(WIT<?> it) {
+        synchronized (iterators) {
+            iterators.add(it);
+        }
+    }
+
+    /**
+     * Unregisters the given iterator from the internal {@code Map}-store.
+     *
+     * @param it {@link WIT}
+     */
+    protected void removeIterator(WIT<?> it) {
+        synchronized (iterators) {
+            iterators.remove(it);
+        }
+    }
+
+    /**
+     * A {@code WrappedIterator} with timestamp and possibility to change base iterator.
+     *
+     * @param <X> anything
+     */
+    public class WIT<X> extends WrappedIterator<X> {
+        protected volatile ExtendedIterator<X> base;
+        protected final Thread thread;
+        protected long timestamp;
+
+        protected WIT(ExtendedIterator<X> base) {
+            super(Objects.requireNonNull(base));
+            this.base = base;
+            this.thread = Thread.currentThread();
+            this.timestamp = currentTimeInMilliSeconds();
+            putIterator(this);
+        }
+
+        protected ExtendedIterator<X> base() {
+            while (base == null) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new IllegalStateException("Unexpected interruption", e);
+                }
+            }
+            return base;
+        }
+
+        protected ExtendedIterator<X> setBase(ExtendedIterator<X> base) {
+            ExtendedIterator<X> res = this.base;
+            this.base = base;
+            return res;
+        }
+
+        protected void refreshTimestamp() {
+            this.timestamp = currentTimeInMilliSeconds();
+        }
+
+        protected long timestamp() {
+            return timestamp;
+        }
+
+        protected boolean isExpired() {
+            return currentTimeInMilliSeconds() - timestamp > delay;
+        }
+
+        protected void deleteFromCollection() {
+            removeIterator(this);
+        }
+
+        @Override
+        public boolean hasNext() {
+            refreshTimestamp();
+            boolean res = base().hasNext();
+            if (!res) {
+                deleteFromCollection();
+            }
+            return res;
+        }
+
+        @Override
+        public X next() {
+            refreshTimestamp();
+            try {
+                return base().next();
+            } catch (NoSuchElementException e) {
+                deleteFromCollection();
+                throw e;
+            }
+        }
+
+        @Override
+        public void remove() {
+            refreshTimestamp();
+            base().remove();
+        }
+
+        @Override
+        public X removeNext() {
+            X res = next();
+            remove();
+            return res;
+        }
+
+        @Override
+        public <X1 extends X> ExtendedIterator<X> andThen(Iterator<X1> other) {
+            refreshTimestamp();
+            return super.andThen(other);
+        }
+
+        @Override
+        public FilterIterator<X> filterKeep(Predicate<X> test) {
+            refreshTimestamp();
+            return super.filterKeep(test);
+        }
+
+        @Override
+        public FilterIterator<X> filterDrop(Predicate<X> test) {
+            refreshTimestamp();
+            return super.filterDrop(test);
+        }
+
+        @Override
+        public <U> ExtendedIterator<U> mapWith(Function<X, U> map) {
+            refreshTimestamp();
+            return super.mapWith(map);
+        }
+
+        @Override
+        public List<X> toList() {
+            refreshTimestamp();
+            try {
+                return asList(this);
+            } finally {
+                deleteFromCollection();
+            }
+        }
+
+        @Override
+        public Set<X> toSet() {
+            refreshTimestamp();
+            try {
+                return asSet(this);
+            } finally {
+                deleteFromCollection();
+            }
+        }
+
+        @Override
+        public void close() {
+            refreshTimestamp();
+            try {
+                close(base());
+            } finally {
+                deleteFromCollection();
+            }
+        }
+
+        @Override
+        public void forEachRemaining(Consumer<? super X> action) {
+            refreshTimestamp();
+            Objects.requireNonNull(action);
+            try {
+                while (hasNext())
+                    action.accept(next());
+            } finally {
+                deleteFromCollection();
+            }
+        }
+    }
+
 }
---
+++ src/test/java/ru/avicomp/ontapi/tests/jena/RWLockedGraphTest.java
@@ -0,0 +1,146 @@
+/*
+ * This file is part of the ONT API.
+ * The contents of this file are subject to the LGPL License, Version 3.0.
+ * Copyright (c) 2019, Avicomp Services, AO
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ * Alternatively, the contents of this file may be used under the terms of the Apache License, Version 2.0 in which case, the provisions of the Apache License Version 2.0 are applicable instead of those above.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package ru.avicomp.ontapi.tests.jena;
+
+import org.apache.jena.graph.Graph;
+import org.junit.Assert;
+import org.junit.Test;
+import org.semanticweb.owlapi.model.AxiomType;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.avicomp.ontapi.internal.AxiomParserProvider;
+import ru.avicomp.ontapi.internal.ONTObject;
+import ru.avicomp.ontapi.jena.OntModelFactory;
+import ru.avicomp.ontapi.jena.RWLockedGraph;
+import ru.avicomp.ontapi.jena.model.OntClass;
+import ru.avicomp.ontapi.jena.model.OntGraphModel;
+import ru.avicomp.ontapi.jena.utils.Iter;
+import ru.avicomp.ontapi.jena.vocabulary.OWL;
+import ru.avicomp.ontapi.jena.vocabulary.RDF;
+import ru.avicomp.ontapi.utils.ReadWriteUtils;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+/**
+ * To test {@link RWLockedGraph}.
+ * Created by @ssz on 05.04.2019.
+ */
+public class RWLockedGraphTest {
+
+    private static final long TIMEOUT = 10_000; // ms
+    private static final Logger LOGGER = LoggerFactory.getLogger(RWLockedGraphTest.class);
+
+    private static final int THREADS_NUM_1 = 3;
+    private static final int THREADS_NUM_2 = 3;
+
+    private static List<AxiomType<? extends OWLAxiom>> EXCLUDED_TYPES = Arrays.asList(AxiomType.DECLARATION,
+            AxiomType.ANNOTATION_ASSERTION, AxiomType.SUBCLASS_OF);
+    private static Set<AxiomType<? extends OWLAxiom>> CONSIDERED_TYPES = AxiomType.AXIOM_TYPES.stream()
+            .filter(x -> !EXCLUDED_TYPES.contains(x)).collect(Iter.toUnmodifiableSet()); // 453
+
+    private static void testRace(OntGraphModel m) throws ExecutionException, InterruptedException {
+        AtomicBoolean process = new AtomicBoolean(true);
+        int threads = THREADS_NUM_1 + THREADS_NUM_2;
+        ExecutorService service = Executors.newFixedThreadPool(threads);
+        List<Future<?>> res = new ArrayList<>();
+        LOGGER.debug("Start racing");
+        for (int i = 0; i < THREADS_NUM_1; i++)
+            res.add(service.submit(toTask(m, process, RWLockedGraphTest::listAxiomsAndModifyClasses)));
+        for (int i = 0; i < THREADS_NUM_2; i++)
+            res.add(service.submit(toTask(m, process, RWLockedGraphTest::modifyClassesAndListClasses)));
+        service.shutdown();
+        Thread.sleep(TIMEOUT);
+        process.set(false);
+        for (Future<?> f : res) {
+            f.get();
+        }
+        LOGGER.debug("Fin.");
+    }
+
+    private static Runnable toTask(OntGraphModel m, AtomicBoolean ready, Consumer<OntGraphModel> action) {
+        return () -> {
+            while (ready.get()) {
+                try {
+                    action.accept(m);
+                } catch (Exception e) {
+                    ready.set(false);
+                    throw e;
+                }
+            }
+        };
+    }
+
+    private static void listAxiomsAndModifyClasses(OntGraphModel m) {
+        LOGGER.debug("[{}]:::listAxiomsAndModifyClasses", Thread.currentThread().getName());
+        Assert.assertEquals(453, listAxioms(CONSIDERED_TYPES, m).count());
+        int num = 10;
+        for (int i = 0; i < num; i++) {
+            m.createOntClass("C" + i).addComment("X" + i);
+        }
+        for (int i = 0; i < num; i++) {
+            OntClass c = m.getOntClass("C" + i);
+            if (c != null) {
+                m.removeOntObject(c);
+            }
+        }
+    }
+
+    private static void modifyClassesAndListClasses(OntGraphModel m) {
+        LOGGER.debug("[{}]:::modifyClassesAndListClasses", Thread.currentThread().getName());
+        String name = Thread.currentThread().getName();
+        int num = 10;
+        for (int i = 0; i < num; i++) {
+            m.createOntClass(name + "a" + i).addSubClassOf(m.createComplementOf(m.createOntClass(name + "b" + i)));
+        }
+        for (int i = 0; i < num; i++) {
+            m.getOntClass(name + "a" + i).listProperties().toSet().forEach(m::remove);
+            m.removeOntObject(m.getOntClass(name + "b" + i));
+        }
+        long count = m.statements(null, RDF.type, OWL.Class).filter(x -> x.getSubject().isURIResource()).count();
+        Assert.assertTrue("Count: " + count, count >= 100);
+    }
+
+    private static Stream<OWLAxiom> listAxioms(Collection<AxiomType<? extends OWLAxiom>> types, OntGraphModel m) {
+        return types.stream()
+                .map(AxiomParserProvider::get)
+                .flatMap(x -> x.axioms(m)).map(ONTObject::getObject);
+    }
+
+    private static Graph loadPizza() {
+        return ReadWriteUtils.loadResourceTTLFile("/ontapi/pizza.ttl").getGraph();
+    }
+
+    @Test
+    public void testRace() throws Exception {
+        Graph g = loadPizza();
+        Graph gg = new RWLockedGraph(g, new ReentrantReadWriteLock());
+        OntGraphModel m = OntModelFactory.createModel(gg);
+        Instant s = Instant.now();
+        testRace(m);
+        Instant e = Instant.now();
+        LOGGER.debug("Duration: {}", Duration.between(s, e));
+    }
+}
