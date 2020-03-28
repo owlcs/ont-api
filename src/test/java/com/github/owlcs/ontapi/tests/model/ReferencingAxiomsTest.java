7caa209c0270b52270791328af27508988c006f5
szuev <sss.zuev@gmail.com>
2020-03-28 13:09:07 +0300

    ont-api: tests -- improve ReferencingAxiomsTest (more careful checking is added)

--- src/test/java/com/github/owlcs/ontapi/tests/model/ReferencingAxiomsTest.java
+++ src/test/java/com/github/owlcs/ontapi/tests/model/ReferencingAxiomsTest.java
@@ -52,9 +52,13 @@ public class ReferencingAxiomsTest {
         return TestData.values();
     }
 
+    private static OWLOntologyManager newManager() {
+        return OntManagers.createONT();
+    }
+
     @Test
     public void testSearchByClass() {
-        data.doTest(T.CLS, HasClassesInSignature::classesInSignature);
+        data.doTest(T.CLASS, HasClassesInSignature::classesInSignature);
     }
 
     @Test
@@ -62,7 +66,7 @@ public class ReferencingAxiomsTest {
         OWLOntology ont = data.load(newManager());
         Set<OWLLiteral> literals = ont.axioms().flatMap(x -> OwlObjects.objects(OWLLiteral.class, x))
                 .collect(Collectors.toSet());
-        data.getTester(T.LTL).testCounts(ont, x -> literals.stream());
+        data.getTester(T.LITERAL).testCounts(ont, x -> literals.stream());
     }
 
     @Test
@@ -74,53 +78,111 @@ public class ReferencingAxiomsTest {
 
     @Test
     public void testSearchByAnonymousIndividuals() {
-        data.doTest(T.ANI, HasAnonymousIndividuals::anonymousIndividuals);
+        data.doTest(T.ANONYMOUS_INDIVIDUAL, HasAnonymousIndividuals::anonymousIndividuals);
     }
 
     @Test
     public void testSearchByNamedIndividuals() {
-        data.doTest(T.NMI, HasIndividualsInSignature::individualsInSignature);
+        data.doTest(T.NAMED_INDIVIDUAL, HasIndividualsInSignature::individualsInSignature);
     }
 
     @Test
     public void testSearchByDatatypes() {
-        data.doTest(T.DTD, HasDatatypesInSignature::datatypesInSignature);
+        data.doTest(T.DATATYPE, HasDatatypesInSignature::datatypesInSignature);
     }
 
     @Test
     public void testSearchByObjectProperty() {
-        data.doTest(T.OBP, HasObjectPropertiesInSignature::objectPropertiesInSignature);
+        data.doTest(T.OBJECT_PROPERTY, HasObjectPropertiesInSignature::objectPropertiesInSignature);
     }
 
     @Test
     public void testSearchByDatatypeProperty() {
-        data.doTest(T.DTP, HasDatatypesInSignature::datatypesInSignature);
+        data.doTest(T.DATA_PROPERTY, HasDatatypesInSignature::datatypesInSignature);
     }
 
     @Test
     public void testSearchByAnnotationProperty() {
-        data.doTest(T.ANP, HasAnnotationPropertiesInSignature::annotationPropertiesInSignature);
-    }
-
-    private static OWLOntologyManager newManager() {
-        return OntManagers.createONT();
+        data.doTest(T.ANNOTATION_PROPERTY, HasAnnotationPropertiesInSignature::annotationPropertiesInSignature);
     }
 
     enum TestData {
-        PIZZA("/ontapi/pizza.ttl", T.CLS.of(1577, 795), T.LTL.of(120), T.IRI.of(2199, 945), T.ANI.of(),
-                T.NMI.of(31, 23), T.DTD.of(120), T.OBP.of(231, 223), T.DTP.of(120), T.ANP.of(120)),
-        FAMILY("/ontapi/family.ttl", T.CLS.of(342, 236), T.LTL.of(521), T.IRI.of(7543, 2845), T.ANI.of(),
-                T.NMI.of(4214, 2372), T.DTD.of(530), T.OBP.of(1891, 1709), T.DTP.of(530), T.ANP.of(4)),
-        PEOPLE("/ontapi/people.ttl", T.CLS.of(235, 151), T.LTL.of(201, 196), T.IRI.of(999, 409), T.ANI.of(12, 11),
-                T.NMI.of(91, 62), T.DTD.of(197, 196), T.OBP.of(76, 65), T.DTP.of(197, 196), T.ANP.of(206, 201)),
-        CAMERA("/ontapi/camera.ttl", T.CLS.of(60, 47), T.LTL.of(0), T.IRI.of(130, 77), T.ANI.of(),
-                T.NMI.of(7, 6), T.DTD.of(8), T.OBP.of(27, 25), T.DTP.of(8), T.ANP.of()),
-        KOALA("/ontapi/koala.ttl", T.CLS.of(82, 59), T.LTL.of(6), T.IRI.of(144, 76), T.ANI.of(),
-                T.NMI.of(18, 17), T.DTD.of(7), T.OBP.of(24, 23), T.DTP.of(7), T.ANP.of(3)),
-        TRAVEL("/ontapi/travel.ttl", T.CLS.of(167, 115), T.LTL.of(12), T.IRI.of(321, 177), T.ANI.of(12),
-                T.NMI.of(61, 51), T.DTD.of(16), T.OBP.of(41, 36), T.DTP.of(16), T.ANP.of(12)),
-        WINE("/ontapi/wine.ttl", T.CLS.of(576, 462), T.LTL.of(4), T.IRI.of(2127, 911), T.ANI.of(),
-                T.NMI.of(1080, 744), T.DTD.of(5), T.OBP.of(456, 449), T.DTP.of(5), T.ANP.of(3)) {
+        PIZZA("/ontapi/pizza.ttl",
+                T.IRI.of(92474991110L),
+                T.LITERAL.of(5847447319L),
+                T.ANONYMOUS_INDIVIDUAL.of(),
+                T.NAMED_INDIVIDUAL.of(4858697351L),
+                T.CLASS.of(75129089786L),
+                T.DATATYPE.of(5847447319L),
+                T.OBJECT_PROPERTY.of(-5055137984L),
+                T.DATA_PROPERTY.of(5847447319L),
+                T.ANNOTATION_PROPERTY.of(5847447319L)
+        ),
+        FAMILY("/ontapi/family.ttl",
+                T.IRI.of(-7556502204L),
+                T.LITERAL.of(-45686983406L),
+                T.ANONYMOUS_INDIVIDUAL.of(),
+                T.NAMED_INDIVIDUAL.of(123428810197L),
+                T.CLASS.of(-7698608481L),
+                T.DATATYPE.of(-41855234952L),
+                T.OBJECT_PROPERTY.of(-40985808704L),
+                T.DATA_PROPERTY.of(-41855234952L),
+                T.ANNOTATION_PROPERTY.of(83282971L)
+        ),
+        PEOPLE("/ontapi/people.ttl",
+                T.IRI.of(-2052328542L),
+                T.LITERAL.of(14670462876L), // https://github.com/owlcs/owlapi/issues/912
+                T.ANONYMOUS_INDIVIDUAL.of(7259412151L),
+                T.NAMED_INDIVIDUAL.of(10749291192L),
+                T.CLASS.of(-36032894329L),
+                T.DATATYPE.of(12391681305L),
+                T.OBJECT_PROPERTY.of(-16161584024L),
+                T.DATA_PROPERTY.of(12391681305L),
+                T.ANNOTATION_PROPERTY.of(16238983895L)
+        ),
+        CAMERA("/ontapi/camera.ttl",
+                T.IRI.of(34678315922L),
+                T.LITERAL.of(),
+                T.ANONYMOUS_INDIVIDUAL.of(),
+                T.NAMED_INDIVIDUAL.of(129741169L),
+                T.CLASS.of(16668417184L),
+                T.DATATYPE.of(6740474844L),
+                T.OBJECT_PROPERTY.of(-1389590202L),
+                T.DATA_PROPERTY.of(6740474844L),
+                T.ANNOTATION_PROPERTY.of()
+        ),
+        KOALA("/ontapi/koala.ttl",
+                T.IRI.of(4895124448L),
+                T.LITERAL.of(152056289L), // https://github.com/owlcs/owlapi/issues/912
+                T.ANONYMOUS_INDIVIDUAL.of(),
+                T.NAMED_INDIVIDUAL.of(356839630L),
+                T.CLASS.of(-3512598891L),
+                T.DATATYPE.of(1252041014L),
+                T.OBJECT_PROPERTY.of(5550952321L),
+                T.DATA_PROPERTY.of(1252041014L),
+                T.ANNOTATION_PROPERTY.of(2255627747L)
+        ),
+        TRAVEL("/ontapi/travel.ttl",
+                T.IRI.of(-9844999114L),
+                T.LITERAL.of(-3973926788L),
+                T.ANONYMOUS_INDIVIDUAL.of(1475303698L),
+                T.NAMED_INDIVIDUAL.of(-330824597L),
+                T.CLASS.of(9819903166L),
+                T.DATATYPE.of(-5564259158L),
+                T.OBJECT_PROPERTY.of(-1128108216L),
+                T.DATA_PROPERTY.of(-5564259158L),
+                T.ANNOTATION_PROPERTY.of(-3973926788L)
+        ),
+        WINE("/ontapi/wine.ttl",
+                T.IRI.of(141385965517L),
+                T.LITERAL.of(3321372063L),
+                T.ANONYMOUS_INDIVIDUAL.of(),
+                T.NAMED_INDIVIDUAL.of(43657818377L),
+                T.CLASS.of(33981903885L),
+                T.DATATYPE.of(2653283947L),
+                T.OBJECT_PROPERTY.of(59036259728L),
+                T.DATA_PROPERTY.of(2653283947L),
+                T.ANNOTATION_PROPERTY.of(1282021579L)) {
             @Override
             String getName() {
                 return "http://www.w3.org/TR/2003/PR-owl-guide-20031209/wine";
@@ -131,8 +193,16 @@ public class ReferencingAxiomsTest {
                 return load(manager, FOOD, this);
             }
         },
-        FOOD("/ontapi/food.ttl", T.CLS.of(415, 284), T.LTL.of(), T.IRI.of(772, 361), T.ANI.of(),
-                T.NMI.of(189, 175), T.DTD.of(), T.OBP.of(168, 107), T.DTP.of(), T.ANP.of()) {
+        FOOD("/ontapi/food.ttl",
+                T.IRI.of(112980744315L),
+                T.LITERAL.of(),
+                T.ANONYMOUS_INDIVIDUAL.of(),
+                T.NAMED_INDIVIDUAL.of(12008713509L),
+                T.CLASS.of(78674545819L),
+                T.DATATYPE.of(),
+                T.OBJECT_PROPERTY.of(22297484987L),
+                T.DATA_PROPERTY.of(),
+                T.ANNOTATION_PROPERTY.of()) {
             @Override
             String getName() {
                 return "http://www.w3.org/TR/2003/PR-owl-guide-20031209/food";
@@ -159,29 +229,6 @@ public class ReferencingAxiomsTest {
             }
             this.format = format;
             this.expectations = expectations;
-        }
-
-        public Tester getTester(T type) {
-            return Arrays.stream(expectations)
-                    .filter(x -> Objects.equals(x.type, type))
-                    .findFirst().orElseThrow(IllegalArgumentException::new);
-        }
-
-        void doTest(T type, Function<OWLOntology, Stream<? extends OWLPrimitive>> getPrimitives) {
-            OWLOntology ont = load(newManager());
-            getTester(type).testCounts(ont, getPrimitives);
-        }
-
-        public OWLOntology load(OWLOntologyManager manager) {
-            try {
-                // no transform
-                OWLOntologyLoaderConfiguration conf = OWLAdapter.get()
-                        .asONT(manager.getOntologyLoaderConfiguration())
-                        .setPerformTransformation(false);
-                return manager.loadOntologyFromOntologyDocument(getDocumentSource(), conf);
-            } catch (OWLOntologyCreationException e) {
-                throw new AssertionError(e);
-            }
         }
 
         static OWLOntology load(OWLOntologyManager manager, TestData... data) {
@@ -213,6 +260,30 @@ public class ReferencingAxiomsTest {
             return res;
         }
 
+        public Tester getTester(T type) {
+            return Arrays.stream(expectations)
+                    .filter(x -> Objects.equals(x.type, type))
+                    .findFirst().orElseThrow(IllegalArgumentException::new);
+        }
+
+        void doTest(T type, Function<OWLOntology, Stream<? extends OWLPrimitive>> getPrimitives) {
+            OWLOntology ont = load(newManager());
+            getTester(type).testCounts(ont, getPrimitives);
+        }
+
+        public OWLOntology load(OWLOntologyManager manager) {
+            try {
+                OWLOntologyLoaderConfiguration conf = manager.getOntologyLoaderConfiguration();
+                if (manager instanceof OntologyManager) {
+                    // no transform
+                    conf = OWLAdapter.get().asONT(conf).setPerformTransformation(false);
+                }
+                return manager.loadOntologyFromOntologyDocument(getDocumentSource(), conf);
+            } catch (OWLOntologyCreationException e) {
+                throw new AssertionError(e);
+            }
+        }
+
         public OWLOntologyDocumentSource getDocumentSource() {
             return new FileDocumentSource(file.toFile(), getDocumentFormat());
         }
@@ -228,59 +299,40 @@ public class ReferencingAxiomsTest {
     }
 
     enum T {
-        CLS,
-        LTL,
-        IRI,
-        ANI,
-        NMI,
-        DTD,
-        OBP,
-        DTP,
-        ANP,
+        IRI, LITERAL, ANONYMOUS_INDIVIDUAL, NAMED_INDIVIDUAL, CLASS, DATATYPE, OBJECT_PROPERTY, DATA_PROPERTY, ANNOTATION_PROPERTY,
         ;
 
-        private Tester of(long sum, long distinct) {
-            return new Tester(this, sum, distinct);
+        private Tester of() {
+            return of(0);
         }
 
         private Tester of(long count) {
-            return of(count, count);
-        }
-
-        private Tester of() {
-            return of(0, 0);
+            return new Tester(this, count);
         }
     }
 
     private static class Tester {
-        private final long distinct;
-        private final long sum;
+        private final long count;
         private final T type;
 
-        private Tester(T type, long sum, long distinct) {
+        private Tester(T type, long count) {
             this.type = type;
-            this.sum = sum;
-            this.distinct = distinct;
+            this.count = count;
         }
 
-        long distinctCount(OWLOntology ont, Stream<? extends OWLPrimitive> stream) {
-            return referencingAxioms(ont, stream).distinct().count();
+        private long calc(OWLAxiom ax) {
+            return ax.anonymousIndividuals().findFirst().isPresent() ?
+                    ax.toString().replaceAll("\\s_:\\w+", " _:x").hashCode() : ax.hashCode();
         }
 
-        long nonDistinctCount(OWLOntology ont, Stream<? extends OWLPrimitive> stream) {
-            return referencingAxioms(ont, stream).count();
-        }
-
-        Stream<? extends OWLAxiom> referencingAxioms(OWLOntology ont, Stream<? extends OWLPrimitive> stream) {
-            return stream.flatMap(ont::referencingAxioms);
+        private Stream<OWLAxiom> referencingAxioms(OWLOntology ont, OWLPrimitive x) {
+            return ont.referencingAxioms(x).distinct();
         }
 
         void testCounts(OWLOntology ont, Function<OWLOntology, Stream<? extends OWLPrimitive>> getPrimitives) {
-            long d = distinctCount(ont, getPrimitives.apply(ont));
-            long s = nonDistinctCount(ont, getPrimitives.apply(ont));
-            String msg = "(" + s + ", " + d + ")";
-            Assert.assertEquals(msg, distinct, d);
-            Assert.assertEquals(msg, sum, s);
+            long res = getPrimitives.apply(ont)
+                    .mapToLong(x -> referencingAxioms(ont, x).mapToLong(this::calc).sum()).sum();
+            Assert.assertEquals(count, res);
         }
     }
 
