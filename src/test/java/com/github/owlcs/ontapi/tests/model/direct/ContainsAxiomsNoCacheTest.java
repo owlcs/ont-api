5f217f178714cd019f837ec5451aecc346231fca
szuev <sss.zuev@gmail.com>
2020-09-22 23:13:26 +0300

    ont-api: add dedicated tests for OWLOntology#containsAxiom(...) (both for content-cache and direct mode) + minor refactoring (see ContainsEntityNoCacheTest)

--- src/test/java/com/github/owlcs/ontapi/tests/model/direct/ContainsAxiomsNoCacheTest.java
+++ src/test/java/com/github/owlcs/ontapi/tests/model/direct/ContainsAxiomsNoCacheTest.java
@@ -12,35 +12,40 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-package com.github.owlcs.ontapi.tests.internal;
+package com.github.owlcs.ontapi.tests.model.direct;
 
 import com.github.owlcs.ontapi.OntManagers;
 import com.github.owlcs.ontapi.OntologyManager;
 import com.github.owlcs.ontapi.config.CacheSettings;
 import com.github.owlcs.ontapi.config.OntConfig;
 import com.github.owlcs.ontapi.tests.ModelData;
-import com.github.owlcs.ontapi.tests.model.ContainsSignatureTest;
+import com.github.owlcs.ontapi.tests.model.ContainsAxiomsTest;
 import org.junit.Assert;
+import org.junit.runners.Parameterized;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 /**
- * To test {@link com.github.owlcs.ontapi.internal.DirectObjectMapImpl}.
- * Created by @ssz on 01.05.2020.
- *
- * @see com.github.owlcs.ontapi.tests.managers.CacheConfigTest;
+ * To test {@link com.github.owlcs.ontapi.internal.DirectObjectMapImpl} for {@link org.semanticweb.owlapi.model.OWLAxiom}.
+ * Created by @ssz on 22.09.2020.
  */
-public class NoComponentCacheSignatureTest extends ContainsSignatureTest {
+public class ContainsAxiomsNoCacheTest extends ContainsAxiomsTest {
 
-    public NoComponentCacheSignatureTest(ModelData data) {
+    public ContainsAxiomsNoCacheTest(ModelData data) {
         super(data);
+    }
+
+    @Parameterized.Parameters(name = "{0}")
+    public static ModelData[] getData() {
+        return new ModelData[]{ModelData.PIZZA, ModelData.FAMILY, ModelData.CAMERA, ModelData.TRAVEL};
     }
 
     @Override
     protected OWLOntologyManager newManager() {
         OntologyManager m = OntManagers.createManager();
         OntConfig conf = m.getOntologyConfigurator()
-                .setModelCacheLevel(CacheSettings.CACHE_COMPONENT, false);
-        Assert.assertFalse(conf.useComponentCache());
+                .setModelCacheLevel(CacheSettings.CACHE_CONTENT, false);
+        Assert.assertFalse(conf.useContentCache());
         return m;
+
     }
 }
