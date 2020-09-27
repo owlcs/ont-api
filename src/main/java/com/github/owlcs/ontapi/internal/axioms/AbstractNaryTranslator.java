7279732a3f2835966ab1d9089bd433c4514c2593
szuev <sss.zuev@gmail.com>
2020-09-27 21:54:30 +0300

    ont-api: add contains&find optimization for OWLNaryAxiom translators (issue #20)

--- src/main/java/com/github/owlcs/ontapi/internal/axioms/AbstractNaryTranslator.java
+++ src/main/java/com/github/owlcs/ontapi/internal/axioms/AbstractNaryTranslator.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of the ONT API.
  * The contents of this file are subject to the LGPL License, Version 3.0.
- * Copyright (c) 2020, The University of Manchester, owl.cs group.
+ * Copyright (c) 2020, owl.cs group.
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
@@ -25,6 +25,7 @@ import com.github.owlcs.ontapi.jena.model.*;
 import com.github.owlcs.ontapi.jena.utils.Iter;
 import com.github.owlcs.ontapi.owlapi.objects.OWLAnonymousIndividualImpl;
 import org.apache.jena.graph.BlankNodeId;
+import org.apache.jena.graph.Triple;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.util.iterator.ExtendedIterator;
@@ -104,6 +105,30 @@ public abstract class AbstractNaryTranslator<Axiom extends OWLAxiom & OWLNaryAxi
 
     protected boolean filter(Statement statement) {
         return statement.getSubject().canAs(getView()) && statement.getObject().canAs(getView());
+    }
+
+    @Override
+    protected Collection<Triple> getSearchTriples(Axiom axiom) {
+        Triple res = getSearchTriple(axiom);
+        return res == null ? Collections.emptyList() :
+                Arrays.asList(res, Triple.create(res.getObject(), res.getPredicate(), res.getSubject()));
+    }
+
+    protected Triple getSearchTriple(Axiom axiom) {
+        if (axiom instanceof WithManyObjects.Simple) {
+            return ((WithManyObjects.Simple<?>) axiom).asTriple();
+        }
+        List<OWL> list = axiom.getOperandsAsList();
+        if (list.size() != 2) {
+            return null; // for Disjoint Objects
+        }
+        OWL left = list.get(0);
+        OWL right = list.get(1);
+        if (left instanceof HasIRI && right instanceof HasIRI) {
+            return Triple.create(WriteHelper.toNode((HasIRI) left),
+                    getPredicate().asNode(), WriteHelper.toNode((HasIRI) right));
+        }
+        return null;
     }
 
     /**
--- src/main/java/com/github/owlcs/ontapi/internal/axioms/AbstractTwoWayNaryTranslator.java
+++ src/main/java/com/github/owlcs/ontapi/internal/axioms/AbstractTwoWayNaryTranslator.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of the ONT API.
  * The contents of this file are subject to the LGPL License, Version 3.0.
- * Copyright (c) 2020, The University of Manchester, owl.cs group.
+ * Copyright (c) 2020, owl.cs group.
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
@@ -22,8 +22,11 @@ import com.github.owlcs.ontapi.jena.model.OntDisjoint;
 import com.github.owlcs.ontapi.jena.model.OntModel;
 import com.github.owlcs.ontapi.jena.model.OntObject;
 import com.github.owlcs.ontapi.jena.model.OntStatement;
+import com.github.owlcs.ontapi.jena.utils.Iter;
 import com.github.owlcs.ontapi.jena.utils.OntModels;
 import com.github.owlcs.ontapi.jena.vocabulary.RDF;
+import org.apache.jena.graph.Graph;
+import org.apache.jena.graph.Triple;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.util.iterator.ExtendedIterator;
@@ -91,6 +94,17 @@ public abstract class AbstractTwoWayNaryTranslator<Axiom extends OWLAxiom & OWLN
     abstract Property getMembersPredicate();
 
     abstract Class<? extends OntDisjoint<ONT>> getDisjointView();
+
+    @Override
+    protected ExtendedIterator<OntStatement> listStatements(OntModel model, AxiomsSettings config, Axiom key) {
+        ExtendedIterator<OntStatement> res = listStatements(model, config);
+        Collection<Triple> search = getSearchTriples(key);
+        if (search.isEmpty()) {
+            return res;
+        }
+        Graph g = model.getBaseGraph();
+        return Iter.create(search).filterKeep(g::contains).mapWith(model::asStatement).andThen(res);
+    }
 
     ONTObject<Axiom> makeAxiom(OntStatement statement,
                                Collection<ONTObject<OWLAnnotation>> annotations,
