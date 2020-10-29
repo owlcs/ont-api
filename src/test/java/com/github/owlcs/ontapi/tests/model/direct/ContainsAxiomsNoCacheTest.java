56df1a2c6bf77ff8bc8d3d2e294204b471668991
szuev <sss.zuev@gmail.com>
2020-10-29 19:50:01 +0300

    ont-api: tests, minor refactoring -- ContainsAxiomsNoCacheTest (replace standalone dynamic testcases with parameterized test)

--- src/test/java/com/github/owlcs/ontapi/tests/model/direct/ContainsAxiomsNoCacheTest.java
+++ src/test/java/com/github/owlcs/ontapi/tests/model/direct/ContainsAxiomsNoCacheTest.java
@@ -32,7 +32,6 @@ import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.SKOS;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -51,90 +50,142 @@ import java.util.stream.Stream;
  */
 public class ContainsAxiomsNoCacheTest extends ContainsAxiomsTest {
 
-    public static Stream<Arguments> data() {
+    public static Stream<Arguments> resourceData() {
         return Stream.of(
-                of(ModelData.PIZZA, OWLDeclarationAxiom.class,
+                of(OWLDeclarationAxiom.class, ModelData.PIZZA,
                         (f, d) -> f.getOWLDeclarationAxiom(f.getOWLObjectProperty(d.getNS() + "hasCountryOfOrigin")))
-                , of(ModelData.PIZZA, OWLSubClassOfAxiom.class, (f, d) -> {
+                , of(OWLSubClassOfAxiom.class, ModelData.PIZZA, (f, d) -> {
                     OWLClass sub = f.getOWLClass(d.getNS() + "PizzaTopping");
                     OWLClass sup = f.getOWLClass(d.getNS() + "Food");
                     return f.getOWLSubClassOfAxiom(sub, sup);
                 })
-                , of(ModelData.PIZZA, OWLDisjointClassesAxiom.class, (f, d) -> {
+                , of(OWLDisjointClassesAxiom.class, ModelData.PIZZA, (f, d) -> {
                     OWLClass c1 = f.getOWLClass(d.getNS() + "American");
                     OWLClass c2 = f.getOWLClass(d.getNS() + "AmericanHot");
                     return f.getOWLDisjointClassesAxiom(c1, c2);
                 })
-                , of(ModelData.TRAVEL, OWLDifferentIndividualsAxiom.class, (f, d) -> {
+                , of(OWLDifferentIndividualsAxiom.class, ModelData.TRAVEL, (f, d) -> {
                     OWLNamedIndividual i1 = f.getOWLNamedIndividual(d.getNS() + "ThreeStarRating");
                     OWLNamedIndividual i2 = f.getOWLNamedIndividual(d.getNS() + "TwoStarRating");
                     return f.getOWLDifferentIndividualsAxiom(i1, i2);
                 })
-                , of(ModelData.PIZZA, OWLObjectPropertyRangeAxiom.class, (f, d) -> {
+                , of(OWLObjectPropertyRangeAxiom.class, ModelData.PIZZA, (f, d) -> {
                     OWLObjectProperty property = f.getOWLObjectProperty(d.getNS() + "hasIngredient");
                     OWLClass clazz = f.getOWLClass(d.getNS() + "Food");
                     return f.getOWLObjectPropertyRangeAxiom(property, clazz);
                 })
-                , of(ModelData.FAMILY, OWLDataPropertyRangeAxiom.class, (f, d) -> {
+                , of(OWLDataPropertyRangeAxiom.class, ModelData.FAMILY, (f, d) -> {
                     OWLDataProperty p = f.getOWLDataProperty(d.getNS() + "hasMarriageYear");
                     OWLDatatype r = f.getIntegerOWLDatatype();
                     return f.getOWLDataPropertyRangeAxiom(p, r);
                 })
-                , of(ModelData.PIZZA, OWLObjectPropertyDomainAxiom.class, (f, d) -> {
+                , of(OWLObjectPropertyDomainAxiom.class, ModelData.PIZZA, (f, d) -> {
                     OWLObjectProperty property = f.getOWLObjectProperty(d.getNS() + "hasIngredient");
                     OWLClass clazz = f.getOWLClass(d.getNS() + "Food");
                     return f.getOWLObjectPropertyDomainAxiom(property, clazz);
                 })
-                , of(ModelData.FAMILY, OWLDataPropertyDomainAxiom.class, (f, d) -> {
+                , of(OWLDataPropertyDomainAxiom.class, ModelData.FAMILY, (f, d) -> {
                     OWLDataProperty p = f.getOWLDataProperty(d.getNS() + "hasMarriageYear");
                     OWLClass c = f.getOWLClass(d.getNS() + "Marriage");
                     return f.getOWLDataPropertyDomainAxiom(p, c);
                 })
-                , of(ModelData.PIZZA, OWLSubObjectPropertyOfAxiom.class, (f, d) -> {
+                , of(OWLSubObjectPropertyOfAxiom.class, ModelData.PIZZA, (f, d) -> {
                     OWLObjectProperty sub = f.getOWLObjectProperty(d.getNS() + "isBaseOf");
                     OWLObjectProperty sup = f.getOWLObjectProperty(d.getNS() + "isIngredientOf");
                     return f.getOWLSubObjectPropertyOfAxiom(sub, sup);
                 })
-                , of(ModelData.FAMILY, OWLSubDataPropertyOfAxiom.class, (f, d) -> {
+                , of(OWLSubDataPropertyOfAxiom.class, ModelData.FAMILY, (f, d) -> {
                     OWLDataProperty sub = f.getOWLDataProperty(d.getNS() + "formerlyKnownAs");
                     OWLDataProperty sup = f.getOWLDataProperty(d.getNS() + "knownAs");
                     return f.getOWLSubDataPropertyOfAxiom(sub, sup);
                 })
-                , of(ModelData.NCBITAXON_CUT, OWLSubAnnotationPropertyOfAxiom.class, (f, d) -> {
+                , of(OWLSubAnnotationPropertyOfAxiom.class, ModelData.NCBITAXON_CUT, (f, d) -> {
                     OWLAnnotationProperty sub = f.getOWLAnnotationProperty(SKOS.editorialNote.getURI());
                     OWLAnnotationProperty sup = f.getOWLAnnotationProperty(SKOS.note.getURI());
                     return f.getOWLSubAnnotationPropertyOfAxiom(sub, sup);
                 })
-                , of(ModelData.NCBITAXON_CUT, OWLAnnotationAssertionAxiom.class, (f, d) -> {
+                , of(OWLAnnotationAssertionAxiom.class, ModelData.NCBITAXON_CUT, (f, d) -> {
                     OWLNamedIndividual s = f.getOWLNamedIndividual(d.getNS() + "46063");
                     OWLAnnotationProperty p = f.getOWLAnnotationProperty(SKOS.prefLabel.getURI());
                     OWLLiteral o = f.getOWLLiteral("Chelone", "en");
                     return f.getOWLAnnotationAssertionAxiom(p, s.getIRI(), o);
                 })
-                , of(ModelData.FAMILY, OWLDataPropertyAssertionAxiom.class, (f, d) -> {
+                , of(OWLDataPropertyAssertionAxiom.class, ModelData.FAMILY, (f, d) -> {
                     OWLNamedIndividual s = f.getOWLNamedIndividual(d.getNS() + "louise_templar_1993");
                     OWLDataProperty p = f.getOWLDataProperty(d.getNS() + "hasBirthYear");
                     OWLLiteral o = f.getOWLLiteral(1993);
                     return f.getOWLDataPropertyAssertionAxiom(p, s, o);
                 })
-                , of(ModelData.FAMILY, OWLObjectPropertyAssertionAxiom.class, (f, d) -> {
+                , of(OWLObjectPropertyAssertionAxiom.class, ModelData.FAMILY, (f, d) -> {
                     OWLNamedIndividual s = f.getOWLNamedIndividual(d.getNS() + "louise_templar_1993");
                     OWLObjectProperty p = f.getOWLObjectProperty(d.getNS() + "hasFather");
                     OWLNamedIndividual o = f.getOWLNamedIndividual(d.getNS() + "stephen_templar_1960");
                     return f.getOWLObjectPropertyAssertionAxiom(p, s, o);
                 })
-                , of(ModelData.FAMILY, OWLClassAssertionAxiom.class, (f, d) -> {
+                , of(OWLClassAssertionAxiom.class, ModelData.FAMILY, (f, d) -> {
                     OWLNamedIndividual s = f.getOWLNamedIndividual(d.getNS() + "louise_templar_1993");
                     OWLClass c = f.getOWLClass(d.getNS() + "Woman");
                     return f.getOWLClassAssertionAxiom(c, s);
                 })
-                , of(ModelData.PIZZA, OWLInverseFunctionalObjectPropertyAxiom.class,
+                , of(OWLInverseFunctionalObjectPropertyAxiom.class, ModelData.PIZZA,
                         (f, d) -> f.getOWLInverseFunctionalObjectPropertyAxiom(f.getOWLObjectProperty(d.getNS() + "isBaseOf")))
         );
     }
 
-    private static <X> Arguments of(ModelData data, Class<X> type, BiFunction<OWLDataFactory, ModelData, X> get) {
-        return Arguments.of(data, type, (Function<OWLDataFactory, X>) f -> get.apply(f, data));
+    public static Stream<Arguments> dynamicData() {
+        String ns = "http://x#";
+        String u1 = ns + "p1";
+        String u2 = ns + "p2";
+        BlankNodeId b1 = BlankNodeId.create();
+        BlankNodeId b2 = BlankNodeId.create();
+        return Stream.of(
+                of(OWLDisjointObjectPropertiesAxiom.class,
+                        m -> createDisjointObjectPropertiesModel(m, ns, u1, u2),
+                        f -> f.getOWLDisjointObjectPropertiesAxiom(f.getOWLObjectProperty(u1), f.getOWLObjectProperty(u2)))
+                , of(OWLSameIndividualAxiom.class,
+                        m -> createSameIndividualsModel(m, ns, b1, b2),
+                        f -> f.getOWLSameIndividualAxiom(f.getOWLAnonymousIndividual(b1), f.getOWLAnonymousIndividual(b2)))
+                , of(OWLDatatypeDefinitionAxiom.class,
+                        m -> createDatatypeDefinitionModel(m, ns, u1, u2),
+                        f -> f.getOWLDatatypeDefinitionAxiom(f.getOWLDatatype(u1), f.getOWLDatatype(u2)))
+        );
+    }
+
+    private static <X> Arguments of(Class<X> type, ModelData data, BiFunction<OWLDataFactory, ModelData, X> createAxiom) {
+        return Arguments.of(type, data, (Function<OWLDataFactory, X>) f -> createAxiom.apply(f, data));
+    }
+
+    private static <X> Arguments of(Class<X> type,
+                                    Function<OntologyManager, OntModel> createModel,
+                                    Function<DataFactory, X> createAxiom) {
+        return Arguments.of(type, createModel, createAxiom);
+    }
+
+    private static OntModel createDisjointObjectPropertiesModel(OntologyManager m, String ns, String p1, String p2) {
+        OntModel res = m.createOntology()
+                .asGraphModel().setNsPrefixes(OntModelFactory.STANDARD).setNsPrefix("x", ns);
+        res.createDisjointObjectProperties(res.createObjectProperty(p1), res.createObjectProperty(p2));
+        return res;
+    }
+
+    private static OntModel createSameIndividualsModel(OntologyManager m, String ns, BlankNodeId b1, BlankNodeId b2) {
+        OntModel res = m.createOntology()
+                .asGraphModel().setNsPrefixes(OntModelFactory.STANDARD).setNsPrefix("x", ns);
+        OntIndividual i1 = res.createResource(new AnonId(b1))
+                .addProperty(RDF.type, res.createOntClass(ns + "C1"))
+                .as(OntIndividual.class);
+        OntIndividual i2 = res.createResource(new AnonId(b2))
+                .addProperty(RDF.type, res.createOntClass(ns + "C2"))
+                .as(OntIndividual.class);
+        i1.addSameAsStatement(i2);
+        return res;
+    }
+
+    private static OntModel createDatatypeDefinitionModel(OntologyManager m, String ns, String d1, String d2) {
+        OntModel res = m.createOntology()
+                .asGraphModel().setNsPrefixes(OntModelFactory.STANDARD).setNsPrefix("x", ns);
+        res.createDatatype(d1).addEquivalentClass(res.createDatatype(d2));
+        return res;
     }
 
     @Override
@@ -151,76 +202,19 @@ public class ContainsAxiomsNoCacheTest extends ContainsAxiomsTest {
         super.testContainsAxioms(data);
     }
 
-    @ParameterizedTest(name = "[{index}] ::: type={1}, model={0}")
-    @MethodSource("data")
-    public <X extends OWLAxiom> void testAxiomTranslator(ModelData data, Class<X> type, Function<OWLDataFactory, X> get) {
+    @ParameterizedTest(name = "[{index}] ::: type={0}, model={1}")
+    @MethodSource("resourceData")
+    public <X extends OWLAxiom> void testAxiomTranslator(Class<X> type, ModelData data, Function<OWLDataFactory, X> get) {
         testAxiomTranslator(m -> ((Ontology) data.fetch(m)).asGraphModel(), type, get);
     }
 
-    @Test
-    public void testDisjointObjectPropertiesAxiomTranslator() {
-        String ns = "http://x#";
-        String p1 = ns + "p1";
-        String p2 = ns + "p2";
-        testAxiomTranslator(m -> createDisjointObjectPropertiesModel((OntologyManager) m, ns, p1, p2)
-                , OWLDisjointObjectPropertiesAxiom.class
-                , f -> f.getOWLDisjointObjectPropertiesAxiom(f.getOWLObjectProperty(p1), f.getOWLObjectProperty(p2)));
-
-        testAxiomTranslator(m -> createDisjointObjectPropertiesModel(m, ns, p1, p2)
-                , OWLDisjointObjectPropertiesAxiom.class);
-    }
-
-    @Test
-    public void testSameIndividualsAxiomTranslator() {
-        String ns = "http://x#";
-        BlankNodeId b1 = BlankNodeId.create();
-        BlankNodeId b2 = BlankNodeId.create();
-        testAxiomTranslator(m -> createSameIndividualsModel((OntologyManager) m, ns, b1, b2)
-                , OWLSameIndividualAxiom.class
-                , f -> {
-                    DataFactory df = (DataFactory) f;
-                    return df.getOWLSameIndividualAxiom(df.getOWLAnonymousIndividual(b1), df.getOWLAnonymousIndividual(b2));
-                });
-
-        testAxiomTranslator(m -> createSameIndividualsModel(m, ns, b1, b2), OWLSameIndividualAxiom.class);
-    }
-
-    @Test
-    public void testDatatypeDefinitionAxiomTranslator() {
-        String ns = "http://x#";
-        String d1 = ns + "d1";
-        String d2 = ns + "d2";
-        testAxiomTranslator(m -> createDatatypeDefinitionModel((OntologyManager) m, ns, d1, d2)
-                , OWLDatatypeDefinitionAxiom.class
-                , f -> f.getOWLDatatypeDefinitionAxiom(f.getOWLDatatype(d1), f.getOWLDatatype(d2)));
-        testAxiomTranslator(m -> createDatatypeDefinitionModel(m, ns, d1, d2), OWLDatatypeDefinitionAxiom.class);
-    }
-
-    private OntModel createDisjointObjectPropertiesModel(OntologyManager m, String ns, String p1, String p2) {
-        OntModel res = m.createOntology()
-                .asGraphModel().setNsPrefixes(OntModelFactory.STANDARD).setNsPrefix("x", ns);
-        res.createDisjointObjectProperties(res.createObjectProperty(p1), res.createObjectProperty(p2));
-        return res;
-    }
-
-    private OntModel createSameIndividualsModel(OntologyManager m, String ns, BlankNodeId b1, BlankNodeId b2) {
-        OntModel res = m.createOntology()
-                .asGraphModel().setNsPrefixes(OntModelFactory.STANDARD).setNsPrefix("x", ns);
-        OntIndividual i1 = res.createResource(new AnonId(b1))
-                .addProperty(RDF.type, res.createOntClass(ns + "C1"))
-                .as(OntIndividual.class);
-        OntIndividual i2 = res.createResource(new AnonId(b2))
-                .addProperty(RDF.type, res.createOntClass(ns + "C2"))
-                .as(OntIndividual.class);
-        i1.addSameAsStatement(i2);
-        return res;
-    }
-
-    private OntModel createDatatypeDefinitionModel(OntologyManager m, String ns, String d1, String d2) {
-        OntModel res = m.createOntology()
-                .asGraphModel().setNsPrefixes(OntModelFactory.STANDARD).setNsPrefix("x", ns);
-        res.createDatatype(d1).addEquivalentClass(res.createDatatype(d2));
-        return res;
+    @ParameterizedTest(name = "[{index}] ::: type={0}")
+    @MethodSource("dynamicData")
+    public <X extends OWLAxiom> void testAxiomTranslator(Class<X> type,
+                                                         Function<OntologyManager, OntModel> createModel,
+                                                         Function<DataFactory, X> createAxiom) {
+        testAxiomTranslator(m -> createModel.apply((OntologyManager) m), type, f -> createAxiom.apply((DataFactory) f));
+        testAxiomTranslator(createModel, type);
     }
 
     private <X extends OWLAxiom> void testAxiomTranslator(Function<OntologyManager, OntModel> create, Class<X> type) {
